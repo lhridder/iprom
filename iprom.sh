08d60df9a93e67f8e7f7d535f69f604cc7a3cdf0
lhridder <63594063+lhridder@users.noreply.github.com>
2021-04-21 15:04:37 +0200

    Added script with aasci art

---
+++ iprom.sh
@@ -0,0 +1,34 @@
+echo "  _____                           ";
+echo " |_   _|                          ";
+echo "   | |  _ __  _ __ ___  _ __ ___  ";
+echo "   | | | '_ \| '__/ _ \| '_ \` _ \ ";
+echo "  _| |_| |_) | | | (_) | | | | | |";
+echo " |_____| .__/|_|  \___/|_| |_| |_|";
+echo "       | |                        ";
+echo "       |_|                        ";
+
+mkdir /srv/nodeexporter
+cd /srv/nodeexporter
+wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-amd64.tar.gz
+tar -xvf node_exporter-1.1.2.linux-amd64.tar.gz
+mv node_exporter-1.1.2.linux-amd64/node_exporter ./
+
+cat > /etc/systemd/system/nodeexporter.service <<- 'EOF'
+[Unit]
+Description=Prometheus
+
+[Service]
+User=root
+Group=root
+WorkingDirectory=/srv/nodeexporter
+ExecStart=/srv/nodeexporter/node_exporter --collector.disable-defaults --collector.cpu --collector.cpufreq --collector.filesystem --collector.meminfo --collector.netdev --collector.time --collector.diskstats --collector.stat
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable --now nodeexporter
+SERVER_IP=$(curl -s http://checkip.amazonaws.com)
+
+echo "Connect this ip to prometheus: $SERVER_IP"
